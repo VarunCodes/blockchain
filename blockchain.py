1349ec80468fed9b483046645b3c6bf080a4e673
Varun Madhukara <mvarun@hotmail.co.uk>
2017-12-22 16:50:09 +0000

    Setting up Flask

--- blockchain.py
+++ blockchain.py
@@ -1,21 +1,19 @@
-"""
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uuid4
-"""
+
+from flask import Flask
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-        # Create new genesis block
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
-        #creates new block and add it to the chain
-
         """
         Create a new Block in the Blockchain
 
@@ -32,15 +30,12 @@ class Blockchain(object):
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
-        # Reset the current list of transactions
         self.current_transactions = []
 
         self.chain.append(block)
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        #adds new transaction to the list of transactions
-
         """
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the Recipient
@@ -75,7 +70,6 @@ class Blockchain(object):
 
     @property
     def last_block(self):
-        #returns the last block in the chain
         return self.chain[-1]
         pass
 
@@ -95,15 +89,40 @@ class Blockchain(object):
         return guess_hash[:4] == "0000"
 
     def hash(block):
-        #hashes a block
-
         """
         Creates a SHA-256 hash of a Block
 
         :param block: <dict> Block
         :return: <str>
         """
-
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+
+# Instantiate our node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instanciate the Blockchain
+blockchain = Blockchain
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
